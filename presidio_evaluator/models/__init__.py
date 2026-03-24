6dfe5a78c78a7c32c97b3c224079b4694d2e60a5
Omri Mendels <omri374@users.noreply.github.com>
2026-03-24 22:25:28 +0200

    feat: [US-010] - Remove non-Presidio model wrappers

--- presidio_evaluator/models/__init__.py
+++ presidio_evaluator/models/__init__.py
@@ -2,17 +2,9 @@
 from .base_model import BaseModel
 from .presidio_analyzer_wrapper import PresidioAnalyzerWrapper
 from .presidio_recognizer_wrapper import PresidioRecognizerWrapper
-from .text_analytics_wrapper import TextAnalyticsWrapper
-from .spacy_model import SpacyModel
-from .stanza_model import StanzaModel
-from .flair_model import FlairModel
 
 __all__ = [
     "BaseModel",
     "PresidioRecognizerWrapper",
     "PresidioAnalyzerWrapper",
-    "TextAnalyticsWrapper",
-    "SpacyModel",
-    "StanzaModel",
-    "FlairModel",
 ]
