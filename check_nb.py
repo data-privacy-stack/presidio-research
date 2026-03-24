fd91fce6f75bd287513b1a6e282b068bd3c1c484
Omri Mendels <omri374@users.noreply.github.com>
2026-03-24 22:18:33 +0200

    chore: remove temp files

--- check_nb.py
+++
@@ -1,6 +0,0 @@
-import json
-nb = json.load(open('notebooks/6_Interactive_Entity_Mapping.ipynb'))
-for i, cell in enumerate(nb['cells']):
-    src = ''.join(cell.get('source', []))
-    if 'from_dataset' in src:
-        print(f'Cell {i}: ' + repr(src[:120]))
